508eee8c1878db50c5c7cfaa8d13ce7e36a6d265
NanReven <nanreven@gmail.com>
2024-09-29 09:50:44 +0300

    custom duration type

--- internal/adapter/dto/custom_time.go
+++ internal/adapter/dto/custom_time.go
@@ -6,24 +6,46 @@ import (
 	"time"
 )
 
+const (
+	timeLayout     = "02.01.2006 15:04:05"
+	durationLayout = "15:04:05"
+)
+
 type CustomTime struct {
 	time.Time
 }
 
-const layout = "02.01.2006 15:04:05"
+type CustomDuration struct {
+	time.Time
+}
 
 func (ct CustomTime) MarshalJSON() ([]byte, error) {
-	time := ct.Time.Format(layout)
+	time := ct.Time.Format(timeLayout)
+	return []byte(`"` + time + `"`), nil
+}
+
+func (cd CustomDuration) MarshalJSON() ([]byte, error) {
+	time := cd.Time.Format(durationLayout)
 	return []byte(`"` + time + `"`), nil
 }
 
 func (ct *CustomTime) UnmarshalJSON(b []byte) error {
 	str := string(b[1 : len(b)-1])
-	time, err := time.Parse(layout, str)
+	time, err := time.Parse(timeLayout, str)
 	if err != nil {
 		return err
 	}
 	ct.Time = time
+	return nil
+}
+
+func (cd *CustomDuration) UnmarshalJSON(b []byte) error {
+	str := string(b[1 : len(b)-1])
+	time, err := time.Parse(durationLayout, str)
+	if err != nil {
+		return err
+	}
+	cd.Time = time
 	return nil
 }
 
@@ -37,6 +59,20 @@ func (ct *CustomTime) Scan(src interface{}) error {
 	return nil
 }
 
+func (cd *CustomDuration) Scan(src interface{}) error {
+	switch value := src.(type) {
+	case time.Time:
+		cd.Time = value
+	default:
+		return errors.New("invalid duration")
+	}
+	return nil
+}
+
 func (ct CustomTime) Value() (driver.Value, error) {
 	return ct.Time, nil
+}
+
+func (cd CustomDuration) Value() (driver.Value, error) {
+	return cd.Time, nil
 }
--- internal/adapter/dto/service_response.go
+++ internal/adapter/dto/service_response.go
@@ -1,8 +1,8 @@
 package dto
 
 type ServiceResponse struct {
-	Category string     `db:"category_title"`
-	Title    string     `db:"service_title"`
-	Duration CustomTime `db:"duration"`
-	Price    float64    `db:"price"`
+	Category string         `db:"category_title"`
+	Title    string         `db:"service_title"`
+	Duration CustomDuration `db:"duration"`
+	Price    float64        `db:"price"`
 }
